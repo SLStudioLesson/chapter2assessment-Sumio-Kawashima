c46bac84c4c029af9338a00381128197c367f969
SumioKawashima <kawashima.sumio@technopro.com>
2024-10-09 11:33:45 +0900

    解答完了

--- app/src/main/java/data/RecipeFileHandler.java
+++ app/src/main/java/data/RecipeFileHandler.java
@@ -1,5 +1,9 @@
 package data;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -16,18 +20,28 @@ public class RecipeFileHandler {
 
     /**
      * 設問1: 一覧表示機能
-     * recipes.txtからレシピデータを読み込み、それをリスト形式で返します。 <br> 
+     * recipes.txtからレシピデータを読み込み、それをリスト形式で返します。 <br>
      * IOExceptionが発生したときは<i>Error reading file: 例外のメッセージ</i>とコンソールに表示します。
      *
      * @return レシピデータ
      */
     public ArrayList<String> readRecipes() {
-        // try {
 
-        // } catch (IOException e) {
-        //     System.out.println("Error reading file:" + e.getMessage());
-        // }
-        return null;
+        // 返り値用のリスト
+        ArrayList<String> recipes = new ArrayList<>();
+
+        String line;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            // ファイルを一行ずつ読み込む
+            while ((line = reader.readLine()) != null) {
+                // レシピをリストに保存
+                recipes.add(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file:" + e.getMessage());
+        }
+
+        return recipes;
     }
 
     /**
@@ -35,15 +49,18 @@ public class RecipeFileHandler {
      * 新しいレシピをrecipes.txtに追加します。<br>
      * レシピ名と材料はカンマ区切りで1行としてファイルに書き込まれます。
      *
-     * @param recipeName レシピ名
+     * @param recipeName  レシピ名
      * @param ingredients 材料名
      */
-     // 
+    //
     public void addRecipe(String recipeName, String ingredients) {
-        // try {
-
-        // } catch (IOException e) {
-
-        // }
+        // 引数のレシピ名と材料名を連結
+        String addSentence = String.join(",", recipeName, ingredients);
+        // ファイル末尾に追加
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
+            writer.write(addSentence + "\n");
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
     }
 }
--- app/src/main/java/ui/RecipeUI.java
+++ app/src/main/java/ui/RecipeUI.java
@@ -37,12 +37,15 @@ public class RecipeUI {
                 switch (choice) {
                     case "1":
                         // 設問1: 一覧表示機能
+                        this.displayRecipes();
                         break;
                     case "2":
                         // 設問2: 新規登録機能
+                        this.addNewRecipe();
                         break;
                     case "3":
                         // 設問3: 検索機能
+                        this.searchRecipe();
                         break;
                     case "4":
                         System.out.println("Exit the application.");
@@ -62,7 +65,24 @@ public class RecipeUI {
      * RecipeFileHandlerから読み込んだレシピデータを整形してコンソールに表示します。
      */
     private void displayRecipes() {
+        // RecipeFileHandler を使用してレシピデータを読み込み
+        ArrayList<String> recipes = fileHandler.readRecipes();
 
+        System.out.println();
+        // レシピが空の場合
+        if (recipes.isEmpty()) {
+            System.out.println("No recipes available.");
+        } else {
+            // レシピ出力
+            System.out.println("Recipes:");
+            System.out.println("-----------------------------------");
+            for (String recipe : recipes) {
+                String[] keyValue = recipe.split(",", 2);
+                System.out.println("Recipe Name: " + keyValue[0]);
+                System.out.println("Main Ingredients: " + keyValue[1]);
+                System.out.println("-----------------------------------");
+            }
+        }
     }
 
     /**
@@ -73,6 +93,21 @@ public class RecipeUI {
      */
     private void addNewRecipe() throws IOException {
 
+        String recipeName;
+        String ingredients;
+
+        // レシピ名入力
+        System.out.print("Enter recipe name: ");
+        recipeName = reader.readLine();
+
+        // 材料入力
+        System.out.print("Enter main ingredients (comma separated): ");
+        ingredients = reader.readLine();
+
+        // ファイルにレシピデータを追加
+        fileHandler.addRecipe(recipeName, ingredients);
+
+        System.out.println("Recipe added successfully.");
     }
 
     /**
@@ -82,8 +117,61 @@ public class RecipeUI {
      * @throws java.io.IOException 入出力が受け付けられない
      */
     private void searchRecipe() throws IOException {
+        // クエリ入力受付
+        System.out.print("Enter search query (e.g., 'name=Tomato&ingredient=Garlic'): ");
+        String query = reader.readLine();
+
+        String queryName = "";
+        String queryIngredient = "";
+        // クエリから検索ワードを抽出
+        if (query.contains("&")) {
+            queryName = query.split("&")[0].split("=")[1];
+            queryIngredient = query.split("&")[1].split("=")[1];
+        } else {
+            // クエリが name or ingredient のみの場合
+            String[] keyValue = query.split("=");
+            switch (keyValue[0]) {
+                case "name":
+                    queryName = keyValue[1];
+                    break;
+                case "ingredient":
+                    queryIngredient = keyValue[1];
+            }
+        }
+
+        // レシピデータ読み込み
+        ArrayList<String> recipes = fileHandler.readRecipes();
+        // 一致するレシピを格納
+        ArrayList<String> matchRecipes = new ArrayList<>();
+
+        // 一致するレシピがあるか検索
+        for (String recipe : recipes) {
+            String[] keyValue = recipe.split(",", 2);
+            // レシピ名が一致
+            if (keyValue[0].contains(queryName)) {
+                // 食材が一致するか判定
+                String[] ingredients = keyValue[1].split(", ");
+                for (String ingredient : ingredients) {
+                    // 食材が一致
+                    if (ingredient.contains(queryIngredient)) {
+                        matchRecipes.add(recipe);
+                    }
+                }
+            }
+        }
+
+        // 検索結果表示
+        System.out.println("Search Results:");
+        // 一致するレシピがない場合
+        if (matchRecipes.isEmpty()) {
+            System.out.println("No recipes found matching the criteria.");
+        } else {
+            // 一致するレシピがある場合
+            for (String matchRecipe : matchRecipes) {
+                System.out.println(matchRecipe);
+            }
+        }
 
     }
 
 }
-
